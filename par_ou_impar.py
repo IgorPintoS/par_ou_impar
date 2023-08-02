a0af76cc83c0a1079a8ba19f7c09e3fae538cc57
Igor Pinto Santos <134015245+IgorPintoS@users.noreply.github.com>
2023-08-02 11:42:15 -0300

    Update and rename ex068.py to par_ou_impar.py

--- par_ou_impar.py
+++ par_ou_impar.py
@@ -1,33 +1,33 @@
-from random import randint
+from random import randint # importando randint para o computador sortear um numero inteiro
 print('=' * 30)
 print('    JOGO DO PAR OU ÍMPAR')
 print('=' * 30)
 cont = 0
-while True:
+while True: # loop que roda enquanto for verdadeiro 
     player = int(input('Digite um valor: '))
-    escolha = str(input('Você quer PAR OU ÍMPAR [P/I]: ')).strip()
+    escolha = str(input('Você quer PAR OU ÍMPAR [P/I]: ')).strip() # variavéis
     computador = randint(1, 10)
     soma = computador + player
-    if soma % 2 == 0 and escolha in 'Pp':
+    if soma % 2 == 0 and escolha in 'Pp': # se você ganhar o loop segue
         print(f'Você jogou {player} e o computador {computador}. O total é {soma} que é PAR!')
         print('Você ganhou.')
         cont += 1
         print('=' * 30)
         print('    Vamos jogar novamente.')
         print('=' * 30)
-    elif soma % 2 != 0 and escolha in 'Ii':
+    elif soma % 2 != 0 and escolha in 'Ii': # mesma coisa acontece aqui, se ganhar o loop segue
         print(f'Você jogou {player} e o computador {computador}. O total é {soma} que é ÍMPAR!')
         print('Você ganhou.')
         cont += 1
         print('=' * 30)
         print('    Vamos jogar novamente.')
         print('=' * 30)
-    elif soma % 2 == 0 and escolha in 'Ii':
+    elif soma % 2 == 0 and escolha in 'Ii': # aqui temos uma parada se você perder
         print(f'Você jogou {player} e o computador {computador}. O total é {soma} que é PAR!')
         print('GAMER OVER.')
         print('=' * 30)
-        break
-    elif soma % 2 != 0 and escolha in 'Pp':
+        break # condição de parada, se o resto da divisão da soma for igual a 0 e a escolha do player for ímpar, o programa para.
+    elif soma % 2 != 0 and escolha in 'Pp': # igual ao de cima, porém agora ao contrário.
         print(f'Você jogou {player} e o computador {computador}. O total é {soma} que é ÍMPAR!')
         print('GAME OVER.')
         print('=' * 30)
